c47a547b2e87ec39ea15f482a1909a56443584dd
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-05-03 18:40:08 +0530

    dsa

--- dsa/notebook/arrays/hard/15-3-sum.py
+++ dsa/notebook/arrays/hard/15-3-sum.py
@@ -35,7 +35,29 @@ Approach:
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        pass
+        target = []
+        n = len(nums)
+        nums.sort()
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            l, r = i+1, n-1
+            while l < r:
+                sums = nums[l] + num + nums[r]
+                if sums > 0:
+                    r -= 1
+                elif sums < 0:
+                    l += 1
+                else:
+                    target.append([num, nums[l], nums[r]])
+                    while l < r and nums [l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums [r] == nums[r-1]:
+                        r -= 1
+                    l +=1
+                    r-=1
+        return target
+                    
 
 class TestThreeSum(unittest.TestCase):
     def setUp(self):
