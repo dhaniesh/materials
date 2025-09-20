c880e87b586a7b2706389768d40bc046866d59d1
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-09-20 22:51:35 +0530

    push

--- notebook/dsa/arrays/medium/2348-number-of-zero-filled-subarrays.py
+++ notebook/dsa/arrays/medium/2348-number-of-zero-filled-subarrays.py
@@ -7,7 +7,7 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 
 Example 1:
 Input: nums = [1,3,0,0,2,0,0,4]
-Output: 6
+Output: 6 
 Explanation: 
 There are 4 occurrences of [0] as a subarray.
 There are 2 occurrences of [0,0] as a subarray.
