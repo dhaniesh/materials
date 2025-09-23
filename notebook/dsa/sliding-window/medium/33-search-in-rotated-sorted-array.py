3bb331cf3bd87cb674b3208124f3ef6d7412e6ca
dhaniesh <53963269+Dhaniesh@users.noreply.github.com>
2025-09-23 15:42:35 +0530

    dsa: 33-search-in-rotated-sorted-array

--- notebook/dsa/sliding-window/medium/33-search-in-rotated-sorted-array.py
+++ notebook/dsa/sliding-window/medium/33-search-in-rotated-sorted-array.py
@@ -30,6 +30,7 @@ Constraints:
 """
 
 import unittest
+from typing import List
 
 """
 Approach:
@@ -40,7 +41,27 @@ and narrow down the search space accordingly.
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        l = 0
+        r = len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+                
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
 
 class TestSearchInRotatedSortedArray(unittest.TestCase):
     def setUp(self):
