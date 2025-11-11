0e31c73be3b6b1a1bbca3474dc65c7041bda1c7b
dhaniesh <53963269+dhaniesh@users.noreply.github.com>
2025-11-11 17:03:13 +0000

    dsa: 704-binary-search

--- notebook/dsa/704-binary-search.py
+++ notebook/dsa/704-binary-search.py
@@ -32,7 +32,16 @@ Approach:
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid -1
+            else:
+                return mid
+        return -1
 
 class TestBinarySearch(unittest.TestCase):
     def setUp(self):
