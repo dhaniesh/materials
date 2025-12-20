e38e106d63144a9294a63e109ba4cdf6ba01a30f
dhaniesh <53963269+dhaniesh@users.noreply.github.com>
2025-12-20 19:35:38 +0530

    dsa: bruteforce 4-median-of-two-sorted-arrays

--- notebook/dsa/4-median-of-two-sorted-arrays.py
+++ notebook/dsa/4-median-of-two-sorted-arrays.py
@@ -3,6 +3,7 @@ from typing import List
 
 
 class Solution:
+    # Brute force: sort and calculate
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         merged = sorted(nums1 + nums2)
         length = len(merged)
